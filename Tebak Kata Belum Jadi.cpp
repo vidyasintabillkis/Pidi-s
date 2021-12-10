d65e6b6d95229db43b06d5679426222b1c289d5f
Vidya Sinta Billkis <92220372+vidyasintabillkis@users.noreply.github.com>
2021-12-10 22:06:25 +0700

    Update Tebak Kata Belum Jadi.cpp

--- Tebak Kata Belum Jadi.cpp
+++ Tebak Kata Belum Jadi.cpp
@@ -1,67 +1,32 @@
 #include <iostream>
-#include <ncurses/ncurses.h>
-#include <windows.h>
-
 using namespace std; 
 
-void header () {
-	initscr();
-	mvprintw(3,15,"GAME TEBAK KATA");
-	mvprintw(4,6,"================================== \n");
+int main (){
+	string jawaban = "TteokBboki"; 
+	string clue = "T__o_B___i"; 
+	string jawab, ya; 
 	
-		mvprintw (5,20, ".");
-		refresh();
-		Sleep(500);
+	
+	do{
+	system ("CLS"); 
+		cout << "Pertanyaan : Salah satu Makanan khas Korea" <<endl;
+		cout << clue <<endl; 
+		 
+		for (int i=0; i<3; i++){
+			cout << "Jawaban Kamu : "; cin >> jawab; 
+		if (jawab==jawaban){
+			cout << "Mantappujiwa"<<endl; 
+			break; 
+		}
 		
-		mvprintw (5,21, ".");
-		refresh();
-		Sleep(500);
+		else {
+			cout <<"zonkk"<<endl; 
+		}
+		cout << "ingin bermain lagi?"; cin >> ya; 
+		}
 		
-		mvprintw (5,22, ".");
-		refresh();
-		Sleep(500);
-		
-		mvprintw (5,23, ".");
-		refresh();
-		Sleep(500);
-		
-		mvprintw (5,24, ".");
-		refresh();
-		Sleep(500);
-	
-	mvprintw(8,6, "Rules : ");
-	mvprintw(9,6, "Kesempatan menjawab hanya diberikan sebanyak 3 kali jika melebihi dianggap kalah ");
-	
-	
-	refresh();
-	getch ();
-	endwin (); 
-}
-
-
-
-int main(){
-	initscr();
-	
-	char jawaban[20] = "Tteok Bokki"; 
-	char strip_jawaban[20] = "_____ _____"; 
-	char tebak; 
-	
-	header ();  
-	clear();
-	
-	mvprintw (3,6, "Terdiri dari 10 huruf Makanan ini merupakan Makanan dari Korea"); 
-	mvprintw (3,6, "Pertanyaan 1"); 
-	
-//	for(int y=0; y<10; y++){
-//		strip_jawaban[y]='_';
-//		mvprintw(13,6, "%c", strip_jawaban[y]);
-//	}	
-	
-	
-	refresh();
-	getch();
-	endwin();
+	}
+	while (ya == "y" || ya == "Y"); 
 	
 	return 0; 
 }
